c1dd449b7792446a6e5f17f3c1e72bdb9469948e
Vlad Soev <sva342007@gmail.com>
2026-02-17 20:16:58 +0200

    add btn+

--- script.js
+++ script.js
@@ -3,16 +3,59 @@ const grid = document.createElement("div");
 grid.className = "grid";
 app.appendChild(grid);
 
-const cols = 4;
+let cols = 4;
 grid.style.gridTemplateColumns = `repeat(${cols}, 50px)`;
 
-const totalCells = 16;
+let rows = 4;
+grid.style.gridTemplateRows = `repeat(${rows}, 50px)`;
+
+const totalCells = rows * cols;
 
 for (let i = 0; i < totalCells; i++) {
   const cell = document.createElement("div");
   cell.className = "cell";
   grid.appendChild(cell);
 }
+
+const addColBtn = document.createElement("button");
+addColBtn.className = "btn add-col";
+addColBtn.textContent = "+";
+
+const addRowBtn = document.createElement("button");
+addRowBtn.className = "btn add-row";
+addRowBtn.textContent = "+";
+
+app.appendChild(addColBtn);
+app.appendChild(addRowBtn);
+
+addColBtn.addEventListener("click", () => {
+  cols = cols + 1;
+  grid.style.gridTemplateColumns = `repeat(${cols}, 50px)`;
+  const totalCells = rows * cols;
+  grid.innerHTML = "";
+  for (let i = 0; i < totalCells; i++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    grid.appendChild(cell);
+  }
+});
+
+addRowBtn.addEventListener("click", () => {
+  rows = rows + 1;
+  grid.style.gridTemplateRows = `repeat(${rows}, 50px)`;
+  const totalCells = rows * cols;
+  grid.innerHTML = "";
+  for (let i = 0; i < totalCells; i++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    grid.appendChild(cell);
+  }
+
+});
+
+
+
+
 
 
 
