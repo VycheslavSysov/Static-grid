64769cdd377cc3b7b1345e2c3d1c4f889d4548e6
Vlad Soev <sva342007@gmail.com>
2026-03-05 14:39:47 +0200

    add row and column changed methods to class

--- script.js
+++ script.js
@@ -78,6 +78,57 @@ class Table {
     this.deleteColumnButton.style.display = "none";
   }
 
+  addNewColumn() {
+    this.columns++;
+    const newColumnIndex = this.columns - 1;
+
+    for (let rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+      const cellElement = this.createCell(rowIndex, newColumnIndex);
+      this.cells[rowIndex].push(cellElement);
+      this.rowElements[rowIndex].appendChild(cellElement);
+    }
+  }
+
+  addNewRow() {
+    this.rows++;
+    const newRowIndex = this.rows - 1;
+    const rowElement = document.createElement("div");
+    rowElement.className = "row";
+    this.tableElement.appendChild(rowElement);
+    this.rowElements[newRowIndex] = rowElement;
+    this.cells[newRowIndex] = [];
+
+    for (let columnIndex = 0; columnIndex < this.columns; columnIndex++) {
+      const cellElement = this.createCell(newRowIndex, columnIndex);
+      this.cells[newRowIndex][columnIndex] = cellElement;
+      rowElement.appendChild(cellElement);
+    }
+  }
+
+  removeRowByIndex(rowIndex) {
+    this.rowElements[rowIndex].remove();
+    this.rowElements.splice(rowIndex, 1);
+    this.cells.splice(rowIndex, 1);
+    this.rows--;
+  }
+
+  removeColumnByIndex(columnIndex) {
+    for (let rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+      this.cells[rowIndex][columnIndex].remove();
+      this.cells[rowIndex].splice(columnIndex, 1);
+    }
+    this.columns--;
+  }
+
+  updateDataCells() {
+    for (let rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+      for (let columnIndex = 0; columnIndex < this.columns; columnIndex++) {
+        this.cells[rowIndex][columnIndex].dataset.row = String(rowIndex);
+        this.cells[rowIndex][columnIndex].dataset.column = String(columnIndex);
+      }
+    }
+  }
+
   renderGrid() {
     this.tableElement.innerHTML = "";
     this.cells.length = 0;
