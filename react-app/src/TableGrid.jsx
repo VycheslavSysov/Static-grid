4a3dfcff0e22e8e692db1a567ab91ccd320b2b76
Vlad Soev <sva342007@gmail.com>
2026-03-11 18:47:42 +0200

    add row/column buttons

--- react-app/src/TableGrid.jsx
+++ react-app/src/TableGrid.jsx
@@ -6,9 +6,23 @@ function TableGrid() {
   const [activeRowIndex, setActiveRowIndex] = useState(null);
   const [activeColumnIndex, setActiveColumnIndex] = useState(null);
 
-  const hanleDeleteRowClick = () => {
+  const handleDeleteRowClick = () => {
     setRowIds((currentIds) => {
       return currentIds.filter((_, index) => index !== activeRowIndex);
+    });
+  };
+
+  const handleAddRowClick = () => {
+    setRowIds((currentIds) => {
+      const lastId = currentIds[currentIds.length - 1];
+      return [...currentIds, lastId + 1];
+    });
+  };
+
+  const handleAddColumnClick = () => {
+    setColumnIds((currentIds) => {
+      const lastId = currentIds[currentIds.length - 1];
+      return [...currentIds, lastId + 1];
     });
   };
 
@@ -18,20 +32,23 @@ function TableGrid() {
           {rowIds.map((rowId, rowIndex) => (
               <div className="row" key={rowId}>
                 {columnIds.map((columnId, columnIndex) => (
-                        <div
-                            className="cell"
-                            key={columnId}
-                            data-row={rowIndex}
-                            data-column={columnIndex}
-                        />
-                    )
-                )
-                }
+                    <div
+                        className="cell"
+                        key={columnId}
+                        data-row={rowIndex}
+                        data-column={columnIndex}
+                    />
+                ))}
               </div>
           ))}
         </div>
+
+        <button className="button add-column" onClick={handleAddColumnClick}>+</button>
+        <button className="button add-row" onClick={handleAddRowClick}>+</button>
+
       </div>
-  )
+  );
+
 }
 
 export default TableGrid;
